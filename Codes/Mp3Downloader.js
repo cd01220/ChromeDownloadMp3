ef01b2fb7a1ffae47a9212ac9b231060d80e0138
cd01220 <10861479@qq.com>
2016-05-16 11:16:58 +0800

    merge from ver.0.1

--- Codes/Mp3Downloader.js
+++ Codes/Mp3Downloader.js
@@ -1,14 +1,27 @@
-sentences = document.getElementById("sentenceSeg");
-
-for (i = 0; i < sentences.childNodes.length; i++)
-{
-    if (sentences.childNodes[i].getAttribute("class") != "se_li")
-        continue;
-    var glfl = sentences.childNodes[i].childNodes[2].childNodes[0]
-
-    var txt = glfl.childNodes[0].getAttribute("onmousedown");   
-    var download = document.createElement("a"); 
-    download.href=txt.match("https.*mp3");
-    download.innerHTML = "download";
-    glfl.appendChild(download);
+function EditSentences()
+{    
+    sentences = document.getElementById("sentenceSeg");
+    for (i = 0; i < sentences.childNodes.length; i++)
+    {
+        if (sentences.childNodes[i].getAttribute("class") != "se_li")
+            continue;
+        var glfl = sentences.childNodes[i].childNodes[2].childNodes[0]
+        if (glfl.childNodes.length < 2)
+        {
+            var txt = glfl.childNodes[0].getAttribute("onmousedown");   
+            var download = document.createElement("a"); 
+            download.href=txt.match("https.*mp3");
+            download.innerHTML = "download";
+            glfl.appendChild(download);
+        }
+    }
 }
+
+function AddButton()
+{
+    table = document.getElementById("sen_bar").childNodes[0];
+    table.onclick = function(){EditSentences();};
+}
+
+AddButton();
+
